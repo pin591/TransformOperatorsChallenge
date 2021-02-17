e64bb98ee75bfa3a3712f756a4931e376f35b464
Ana <anarebollo@xpertai.com>
2021-02-17 12:33:29 +0100

    element at operator example

--- PlaygroundFilltering.playground/Contents.swift
+++ PlaygroundFilltering.playground/Contents.swift
@@ -19,10 +19,27 @@ example(of: "ignoreElements") {
             print($0)
         }.disposed(by: diposeBag)
     
-    cannedProjects.onNext("landOfDroids")
-    cannedProjects.onNext("wookieWorld")
+    cannedProjects.onNext("land of droids")
+    cannedProjects.onNext("wookie world")
     cannedProjects.onNext("detours")
     cannedProjects.onCompleted()
+}
+
+example(of: "elementAt") {
+    
+    let disposeBag = DisposeBag()
+    
+    let quotes = PublishSubject<String>()
+    quotes
+        .elementAt(1)
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
+    quotes.onNext("may the odds")
+    quotes.onNext("live long and prosper")
+    quotes.onNext("may the force")
 }
 
 
