98cd4cc4bccbb31ffac1438875a7309361dea74e
Ana <anarebollo@xpertai.com>
2021-02-17 12:21:33 +0100

    initial basic project

---
+++ InstallingRxSwift/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  InstallingRxSwift
+//
+//  Created by Ana Rebollo Pin on 28/01/2021.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
