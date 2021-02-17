f9599aadebc0cf1dfd32fae3e2de834a33266db3
Ana <anarebollo@xpertai.com>
2021-02-17 12:43:07 +0100

    filter operator example

--- PlaygroundFilltering.playground/Contents.swift
+++ PlaygroundFilltering.playground/Contents.swift
@@ -1,6 +1,17 @@
 import UIKit
 import RxSwift
 
+public let episodeI = (title: "The Phantom Menace", rating: 55)
+public let episodeII = (title: "Attack of the Clones", rating: 66)
+public let episodeIII = (title: "Revenge of the Sith", rating: 79)
+public let rogueOne = (title: "Rogue One", rating: 85)
+public let episodeIV = (title: "A New Hope", rating: 93)
+public let episodeV = (title: "The Empire Strikes Back", rating: 94)
+public let episodeVI = (title: "Return Of The Jedi", rating: 80)
+public let episodeVII = (title: "The Force Awakens", rating: 93)
+public let episodeVIII = (title: "The Last Jedi", rating: 91)
+public let tomatometerRatings = [episodeI, episodeII, episodeIII, rogueOne, episodeIV, episodeV, episodeVI, episodeVII, episodeVIII]
+
 
 public func example(of description: String, action: () -> Void) {
     print ("\n---- Example of:", description, "-----")
@@ -40,6 +51,21 @@ example(of: "elementAt") {
     quotes.onNext("may the odds")
     quotes.onNext("live long and prosper")
     quotes.onNext("may the force")
+}
+
+example(of: "filter") {
+    
+    let disposeBag = DisposeBag()
+    
+    Observable.from(tomatometerRatings)
+        .filter { movie in
+            movie.rating >= 80
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
 }
 
 
