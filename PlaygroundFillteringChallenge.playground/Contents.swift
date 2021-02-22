6884bec32cff9d375d89d8d3f4ebced07bdcbda3
Ana <anarebollo@xpertai.com>
2021-02-22 15:45:47 +0100

    final solution

--- PlaygroundFillteringChallenge.playground/Contents.swift
+++ PlaygroundFillteringChallenge.playground/Contents.swift
@@ -36,7 +36,21 @@ example(of: "Challenge 1") {
   let input = PublishSubject<Int>()
   
   // Add your code here
-  
+  input
+    .skipWhile { $0 == 0 }
+    .filter { $0 < 10 }
+    .take(10)
+    .toArray()
+    .asObservable()
+    .subscribe(onNext: {
+        let phone = phoneNumber(from: $0)
+        if let contact = contacts[phone] {
+            print("Dialing \(contact) (\(phone))...")
+        } else {
+            print("Contact not found")
+        }
+       })
+    .disposed(by: disposeBag)
   
   input.onNext(0)
   input.onNext(603)
@@ -45,7 +59,7 @@ example(of: "Challenge 1") {
   input.onNext(1)
   
   // Confirm that 7 results in "Contact not found", and then change to 2 and confirm that Junior is found
-  input.onNext(7)
+  input.onNext(2)
   
   "5551212".forEach {
     if let number = (Int("\($0)")) {
@@ -55,32 +69,4 @@ example(of: "Challenge 1") {
   
   input.onNext(9)
 }
-
-/*:
- Copyright (c) 2014-2018 Razeware LLC
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
-
-
-
-
-
-
 
